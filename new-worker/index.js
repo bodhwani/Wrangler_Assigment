eddc65d5dd75006e1b0a664e8922b9ced591ce1f
bodhwani <vinitbodhwani123@gmail.com>
2020-10-27 13:15:43 -0700

    Remove redundant code

--- new-worker/index.js
+++ new-worker/index.js
@@ -1,15 +1,3 @@
-// addEventListener('fetch', event => {
-//   event.respondWith(handleRequest(event.request))
-// })
-// /**
-//  * Respond with hello worker text
-//  * @param {Request} request
-//  */
-// async function handleRequest(request) {
-//   return new Response('Hello worker!', {
-//     headers: { 'content-type': 'text/plain' },
-//   })
-// }
 addEventListener("fetch", event => {
   const links =[
     {
@@ -20,10 +8,7 @@ addEventListener("fetch", event => {
         "name": "Linkedin" ,
         "url" : "https://www.linkedin.com/in/niranjaniwagh/"
     },
-    {
-        "name": "Portfolio" ,
-        "url" : "https://niranjani29.github.io/"
-    },
+    
   ]
   const json = JSON.stringify(links, null, 2)
 
